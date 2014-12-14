52c3d3ce901e4f2a006ebc77466993196b66039c
Yue Chao Qin <yqin100@gmail.com>
2014-12-14 18:06:54 -0500

    Finished program

--- CollegeBoard/NumberGuessingGame/src/main/java/com/collegeboard/game/NumberGuess.java
+++ CollegeBoard/NumberGuessingGame/src/main/java/com/collegeboard/game/NumberGuess.java
@@ -1,5 +1,9 @@
 package com.collegeboard.game;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,6 +35,10 @@ public class NumberGuess {
      * Maximum number for the guessing game.  Default value = 2147483647.
      */
     private int maxNumber = Integer.MAX_VALUE;
+    /**
+     * Object to read user input.
+     */
+	private Scanner scanner = new Scanner(System.in);
     
     /**
      * Contructor for the number-guessing game.
@@ -38,9 +46,11 @@ public class NumberGuess {
      */
 	public NumberGuess(String[] args) {
 		try {
-			log.info("Welcome to the Number Guessing Game");
+			System.out.println("Welcome to the Number Guessing Game");
+			log.info("Starting new game");
 			init(args);
 		} catch (Throwable e) {
+			System.out.println("ERROR (stopping game...) => " + e.getMessage());
 			log.error(e.getMessage(),e);
 		}
 	}
@@ -57,19 +67,120 @@ public class NumberGuess {
 			if (maxNumber < minNumber) {
 				throw new NumberGuessException(usage + "  Number has to be >= " + minNumber);
 			}
+			this.maxNumber = maxNumber; 
 		}
 		else if (args.length > 1) {
 			throw new NumberGuessException(usage + "  Provide a positive number for the max value in the guessing game.  If omitted, the max number is " + maxNumber);
 		}	
+	}
+	
+	/**
+	 * Display rules to user.
+	 */
+	private void displayRules() {		
+		System.out.println("Rules: \t\t1) Please think of a number from " + minNumber + " to " + maxNumber);
+		System.out.println("\t\t2) Type 'higher' if your number is higher");
+		System.out.println("\t\t3) Type 'lower' if your number is lower");
+		System.out.println("\t\t4) Type 'yes' if the number matches your number");
+		System.out.println("\t\t4) Type 'end' to end the game");
+		System.out.println("\t\t*** the input strings are case sensitive, lower case only! ***");
+	}
+	
+	/**
+	 * Starts the game with instructions and user input.
+	 * @throws NumberGuessException Exception thrown if the new guess number has a minimum number that greater than the maximum number.
+	 */
+	public void start() throws NumberGuessException {
+		displayRules();
 		
-		log.info("Please think of a number from " + minNumber + " to " + maxNumber);
+		System.out.println("Hit ENTER to start game...");
+		scanner.nextLine();
+		startGame();
+	}
+	
+	/**
+	 * Start the game logic.
+	 * @throws NumberGuessException Exception thrown if the new guess number has a minimum number that greater than the maximum number.
+	 */
+	void startGame() throws NumberGuessException {
+		boolean found = false;
+		boolean checkPastGuessValues = true;
+		int lastMaxNumber = maxNumber;
+		int lastMinNumber = minNumber;
+		int guessNumber = lastMaxNumber / 2;
+		int count = 1;
+		Set<Integer> lastValues = new HashSet<Integer>();
+		
+		while (!found) {
+			log.info("Guess number " + guessNumber);
+			if (checkPastGuessValues) {
+				if (lastValues.contains(guessNumber)) {
+					System.out.println("We already presented this number to you => " + guessNumber + ",  You probably forgot your number, ending game now...");
+					log.error("User was already presented with number => " + guessNumber + ", ending game.");
+					break;
+				}
+				else {
+					lastValues.add(guessNumber);
+				}
+				count++;
+			}
+			checkPastGuessValues = true;
+
+			System.out.println(count + ") Is your number " + guessNumber + "?");
+			String userInput = scanner.next();
+			switch (userInput) {
+				case "higher" :
+					// do higher
+					lastMinNumber = guessNumber;
+					guessNumber = getNewGuessNumber(lastMinNumber, lastMaxNumber);
+					break;
+				case "lower" :
+					// do lower
+					lastMaxNumber = guessNumber;
+					guessNumber = getNewGuessNumber(lastMinNumber, lastMaxNumber);
+					break;
+				case "yes" :
+					found = true;
+					System.out.println("*** Found your number => " + guessNumber + " ***");		
+					log.info("Found number => " + guessNumber);
+					break;
+				case "end" :
+					found = true;
+					System.out.println("Ending game...");
+					log.info("Quitting game...");
+					break;
+				default:
+					checkPastGuessValues = false;
+					System.out.println("Do not recognize option '" + userInput + "'.  Please type one of the following options (case sensitive) ['higher', 'lower', 'yes', 'end'].");
+					log.error("User input '" + userInput + "' not valid");
+			}
+		}
+		
+		System.out.println("Thanks for playing!");
+		log.info("Game finished");
+	}
+	
+	/**
+	 * Get the next guess number, should be a number between the min/max number.
+	 * @param lastMinNumber Minimum number, should be smaller than max.
+	 * @param lastMaxNumber Maximum number, should be greater than min.
+	 * @return The number between min/max.
+	 * @throws NumberGuessException Exception thrown when min is greater than max.
+	 */
+	int getNewGuessNumber(int lastMinNumber, int lastMaxNumber) throws NumberGuessException {
+		if (lastMinNumber > lastMaxNumber) {
+			throw new NumberGuessException("The min (" + lastMinNumber + ") number has to be smaller than the max (" + lastMaxNumber + ") number.");
+		}
+		return lastMinNumber + ((lastMaxNumber - lastMinNumber) / 2);
 	}
 	
 	/**
 	 * Start of program.
 	 * @param args User arguments.
+	 * @throws NumberGuessException Exception thrown if the new guess number has a minimum number that greater than the maximum number.
 	 */
-	public static void main(String[] args) {
-		new NumberGuess(args);
+	public static void main(String[] args) throws NumberGuessException {
+		NumberGuess numberGuess = new NumberGuess(args);
+		numberGuess.start();
 	}
 }
--- CollegeBoard/NumberGuessingGame/src/test/java/com/collegeboard/game/NumberGuessTest.java
+++ CollegeBoard/NumberGuessingGame/src/test/java/com/collegeboard/game/NumberGuessTest.java
@@ -1,11 +1,18 @@
 package com.collegeboard.game;
 
+import java.io.IOException;
+
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 
 import com.collegeboard.exception.NumberGuessException;
 
 public class NumberGuessTest {
+	@Rule
+	public final TextFromStandardInputStream systemInMock = TextFromStandardInputStream.emptyStandardInputStream();
+	
 	@Test
 	public void testInitZeroArgs() {
 		try {
@@ -70,5 +77,70 @@ public class NumberGuessTest {
 		} catch (NumberGuessException e) {
 			Assert.assertTrue(true);
 		}		
+	}
+	
+	@Test
+	public void testGetNewGuessNumberException() {
+		String[] args = new String[] {};
+		NumberGuess numberGuess = new NumberGuess(args);
+		int lastMinNumber;
+		int lastMaxNumber;
+
+		lastMinNumber = 100;
+		lastMaxNumber = 1;
+		try {
+			numberGuess.getNewGuessNumber(lastMinNumber, lastMaxNumber);
+			Assert.fail("Min number " + lastMinNumber + " cannot be larger than max number " + lastMaxNumber);
+		} catch (NumberGuessException e) {
+			Assert.assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void testGetNewGuessNumber() throws NumberGuessException {
+		String[] args = new String[] {};
+		NumberGuess numberGuess = new NumberGuess(args);
+		int lastMinNumber;
+		int lastMaxNumber;
+
+		lastMinNumber = 52;
+		lastMaxNumber = 67;
+		Assert.assertEquals(59, numberGuess.getNewGuessNumber(lastMinNumber, lastMaxNumber));
+		lastMinNumber = 52;
+		lastMaxNumber = 54;
+		Assert.assertEquals(53, numberGuess.getNewGuessNumber(lastMinNumber, lastMaxNumber));
+		lastMinNumber = 52;
+		lastMaxNumber = 53;
+		Assert.assertEquals(52, numberGuess.getNewGuessNumber(lastMinNumber, lastMaxNumber));
+	}
+	
+	@Test
+	public void testGameEnd() throws NumberGuessException, IOException, InterruptedException {
+		String[] args = new String[] {"100"};
+
+		systemInMock.provideText("start\r\n"
+				+ "end\r\n");
+		NumberGuess numberGuess = new NumberGuess(args);
+		try {
+			numberGuess.start();
+			Assert.assertTrue(true);
+		} catch (NumberGuessException e) {
+			Assert.fail();
+		}
+	}
+	
+	@Test
+	public void testGameYes() throws NumberGuessException, IOException, InterruptedException {
+		String[] args = new String[] {"100"};
+
+		systemInMock.provideText("start\r\n"
+				+ "yes\r\n");
+		NumberGuess numberGuess = new NumberGuess(args);
+		try {
+			numberGuess.start();
+			Assert.assertTrue(true);
+		} catch (NumberGuessException e) {
+			Assert.fail();
+		}
 	}
 }
